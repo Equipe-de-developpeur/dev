bf846f4739e5f5f6e38ae2a67e9b68d0c66adcce
Kenji <DWWM-kenji@mode83.onmicrosoft.com>
2020-06-17 14:57:33 +0200

    commit KN

---
+++ KN_passion/js/effets.js
@@ -0,0 +1,107 @@
+window.onload = function()
+{
+	
+	// N01
+    // Ecouteur d'évènement sur scroll
+    window.addEventListener("scroll", () => {
+        // Calcul de la hauteur "utile" du document
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight
+
+        // Récupération de la position verticale
+        let position = window.scrollY
+
+        // Récupération de la largeur de la fenêtre
+        let largeur = document.documentElement.clientWidth
+
+        // Calcul de la largeur de la barre
+        let barre = position / hauteur * largeur
+
+        // Modification du CSS de la barre
+        document.getElementById("progress01").style.width = barre+"px"
+    })
+	
+	
+	//N02
+	 // Ecouteur d'évènement sur scroll
+    window.addEventListener("scroll", () => {
+        // Calcul de la hauteur "utile" du document
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight
+
+        // Récupération de la position verticale
+        let position = window.scrollY
+
+        // Récupération de la largeur de la fenêtre
+        let largeur = document.documentElement.clientWidth
+
+        // Calcul de la largeur de la barre
+        let barre = position / hauteur * largeur
+
+        // Modification du CSS de la barre
+        document.getElementById("progress02").style.width = barre+"px"
+    })
+	
+	//N03
+	 // Ecouteur d'évènement sur scroll
+    window.addEventListener("scroll", () => {
+        // Calcul de la hauteur "utile" du document
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight
+
+        // Récupération de la position verticale
+        let position = window.scrollY
+
+        // Récupération de la largeur de la fenêtre
+        let largeur = document.documentElement.clientWidth
+
+        // Calcul de la largeur de la barre
+        let barre = position / hauteur * largeur
+
+        // Modification du CSS de la barre
+        document.getElementById("progress03").style.width = barre+"px"
+    })
+
+	//Get the button
+	var mybutton = document.getElementById("myBtn");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
+		mybutton.style.display = "block";
+		} else {
+		mybutton.style.display = "none";
+		}
+	}
+	
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function(){
+    $(window).on('scroll', function () {
+    
+    var elmt   = $('.from-left, .from-right');
+    var topImg = $('.from-left, .from-right').offset().top;
+    var scroll = $(window).scrollTop();
+    
+        $(elmt).each(function() {
+            
+            var topImg = $(this).offset().top - 300;
+            
+                if ( topImg < scroll ) {
+                
+                $(this).css("transform", "translate(0,0)");
+                $(this).css("opacity", "1");
+                
+                };
+        });
+    });
+});
+
+
+
+
