f27021ae13376cf7160cceb39850f17ed0d532c2
marie-Mauduit <mhmauduit@hotmail.fr>
2020-07-03 14:53:12 +0200

    changement target_blank

---
+++ page-1.js
@@ -0,0 +1,18 @@
+
+window.onload = function()
+{
+// Effet machine à écrire
+    function extraire() {
+        character = message.substring(I, I=I+1) ;
+        if ( character == "-" &&  message.substr(I, 5) == "stop-" ) {             
+            character = "<br>" ;
+            I = I + 5;
+        }
+        auto_text.innerHTML += character;
+        if ( I < message.length ) setTimeout(extraire, 100);
+    }
+    I = 0 ;
+    message = "J'aime cuisiner pour ma Famille et mes Amis...";
+    extraire();
+}
+
