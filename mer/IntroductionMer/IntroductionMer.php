81a95b2e1dd706e10512998efd56252a2b4d45e7
Aurélien <dupuis.aurelien.eps@outlook.fr>
2020-08-18 11:04:18 +0200

    Rectification de L'introductionMer

--- mer/IntroductionMer/IntroductionMer.php
+++ mer/IntroductionMer/IntroductionMer.php
@@ -1,10 +1,10 @@
-  <div class="IntroductionMer">
-    <h2>
+<div class="IntroductionMer">
+    <h2 style="background-image: url(img/fond-eau-plan);">
       Introduction
     </h2>
     <div class="Contenue">
       <p class="Texte">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis eligendi inventore veniam quos officiis aliquid perspiciatis tempore ad libero commodi numquam officia laudantium quasi sapiente non tenetur ipsam, consequuntur eum ducimus voluptatibus. Eos minima ipsam officiis harum quae consectetur impedit dignissimos ut cupiditate! Debitis dolorem nihil fugiat voluptates enim sed eligendi qui ipsam accusamus, veritatis eos nulla hic maiores, quae perferendis! Odio, magni dignissimos distinctio tempore nisi provident repudiandae accusantium animi, fugit illum eligendi, sint pariatur. Laborum a modi et consequatur voluptates nisi officia quibusdam, ab quidem totam quasi itaque eius! Quia dicta ipsum quisquam iure consectetur? Illo, cupiditate dolor?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis eligendi inventore veniam quos officiis aliquid perspiciatis tempore ad libero commodi numquam lorem lorem.
       </p>
       <div class="Partager">
         <a href="http://(direction vers les articles)" target="_blank" rel="noopener noreferrer">Partager
@@ -12,3 +12,7 @@
       </div>
     </div>
   </div>
+
+
+
+  
--- mer/IntroductionMer/IntroductionMer_Réception.php
+++ mer/IntroductionMer/IntroductionMer_Réception.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" style="text/css" href="IntroductionMer.css">
-  <link rel="stylesheet" style="text/css" href="IntroductionMer_Mobile.css">
   <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed&display=swap" rel="stylesheet">
 
   <title>Introduction Mer</title>
@@ -12,17 +11,15 @@
 
 
 
-<body style="background-image: url(img/HeaderMer.jpg);">
-<div style="margin-top: 200px;">
+<body style="background: url(img/fond_eau_plan.jpg); background-repeat: no-repeat; background-size: cover;">
+<div style="margin-top: 10vw;">
 
 </div>
 <?php
-include ('IntroductionMer.php');
+include 'IntroductionMer.php';
 ?>
+
 
 
 </body>
 </html>
-
-
-
