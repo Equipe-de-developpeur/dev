b078629d7d17125c3d7cc914d56fc8c1c7859850
Aurélien <dupuis.aurelien.eps@outlook.fr>
2020-08-18 15:57:09 +0200

    organisation + renommage

--- mer/introduction/php/DA_mer_introduction(réception).php
+++ mer/introduction/php/DA_mer_introduction(réception).php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" style="text/css" href="IntroductionMer.css">
+  <link rel="stylesheet" style="text/css" href="../css/DA_mer_introduction.css">
   <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed&display=swap" rel="stylesheet">
 
   <title>Introduction Mer</title>
@@ -11,12 +11,12 @@
 
 
 
-<body style="background: url(img/fond_eau_plan.jpg); background-repeat: no-repeat; background-size: cover;">
+<body style="background: url(../../img/fond_eau_plan.jpg); background-repeat: no-repeat; background-size: cover;">
 <div style="margin-top: 10vw;">
 
 </div>
 <?php
-include 'IntroductionMer.php';
+include 'DA_mer_introduction.php';
 ?>
 
 
