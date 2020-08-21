e2ba389e9b7960e4ffdd6a8879057b0253c40542
ayala <balbunho@gmail.com>
2020-08-21 16:48:44 +0200

    activité

---
+++ activite/header.php
@@ -0,0 +1,15 @@
+<Header class="container-fluid">
+<img id="logo" src="image/logo-09.png">
+
+<div id="intro" class="container" "><img class="col-lg-3 col-md-3 col-sm-3 col-3" src="image/10046.jpg"><p class="col-lg-9 col-md-9 col-sm-9 col-9"">Lorem ipsum dolor sit amet,
+     consectetur adipiscing elit. Suspendisse non ante hendrerit,
+      porta mauris eu, facilisis sem. Proin quam tellus, viverra sit amet
+       erat vitae, pretium aliquet ipsum. Vivamus dictum quam eget sem hendrerit
+    convallis</p>
+    <button>DONNER VOTRE AVIS</button>
+</div>
+
+<h5>ACTIVITES</h5>
+
+</Header>
+ 
