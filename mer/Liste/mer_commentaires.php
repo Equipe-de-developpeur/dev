2db846b5e9ddebf257ad0f467b388a37cec598ec
Aurélien <dupuis.aurelien.eps@outlook.fr>
2020-08-27 11:25:12 +0200

    formulaire visuel ok

--- mer/Liste/mer_commentaires.php
+++ mer/Liste/mer_commentaires.php
@@ -1,27 +1,51 @@
-
 <section id="CommentairesMer">
+
+  <!--Titre-->
+
   <div class="TitreCommentairesMer">
     <h2>Commentaires :</h2>
   </div>
+
+  <!--Section Commentaires-->
+
   <div class="SectionCommentairesMer">
+
+    <!--Base Commentaires-->
+
     <div class="BaseCommentairesMer">
+
+      <!--Début Commentaires-->
+
       <div class="DebutCommentairesMer">
         Pas content
       </div>
+
+      <!--Réponse Commentaires-->
+
       <div class="ReponseCommentairesMer">
         Pourquoi pas content ?
       </div>
     </div>
+
+    <!--Recherche Commentaires-->
+
     <div class="RechercheCommentairesMer">
-      <div>
-        <a href="">RECHERCHE</a>
-      </div>
-      <div class="ChampDeRechercheCommentairesMer">
-        <input type="text">
-      </div>
+
+      <form method="post" action="header.php">
+
+        <!--Envoi de la Recherche Commentaires-->
+
+        <div class="ChampEnvoiCommentairesMer">
+          <input type="submit" value="RECHERCHE">
+        </div>
+
+        <!--Champ Recherche Commentaires-->
+
+        <div class="ChampDeRechercheCommentairesMer">
+          <input type="text" id="commentaires" name="commentaires" maxlength="30" placeholder="Recherche" autocomplete="">
+        </div>
+      </form>
+
     </div>
-
   </div>
-
-
 </section>
--- mer/Liste/mer_listes.php
+++ mer/Liste/mer_listes.php
@@ -1,11 +1,95 @@
+<!--Listes Mer-->
+
 <section id="ListesMer">
+
+  <!--Titre-->
+
   <div class="TitreListesMer">
     <H2>LISTES DES PLAGES / PORTS / ILES ECO RESPONSABLE</H2>
   </div>
 
+  <!--Recherche-->
+
   <div class="RechercheListesMer">
-    RECHERCHE
+
+    <form method="post" action="header.php">
+
+      <!--input lieux-->
+
+      <select id="lieux" name="lieux">
+        <option value="lieux">Lieux</option>
+        <optgroup label="Plages">
+          <option value="Saint-Clair">Saint-Clair</option>
+          <option value="l'Escalet">l'Escalet</option>
+          <option value="la Verne">la Verne</option>
+        </optgroup>
+        <optgroup label="Ports">
+          <option value="Oursinières">Oursinières</option>
+          <option value="Issambres">Issambres</option>
+          <option value="Brusc">Brusc</option>
+        </optgroup>
+        <optgroup label="Iles">
+          <option value="Notre-Dame">Notre-Dame</option>
+          <option value="d’Argent">d’Argent</option>
+          <option value="Noire du Langoustier">Noire du Langoustier</option>
+        </optgroup>
+      </select>
+
+      <!--input villes-->
+
+      <select id="villes" name="villes">
+        <option value="villes">Villes</option>
+        <optgroup label="Plages">
+          <option value="Fréjus">Fréjus</option>
+          <option value="Saint Raphael">Saint Raphael</option>
+          <option value="Agay">Agay</option>
+        </optgroup>
+        <optgroup label="Ports">
+          <option value="Fréjus">Fréjus</option>
+          <option value="Saint Raphael">Saint Raphael</option>
+          <option value="Agay">Agay</option>
+        </optgroup>
+        <optgroup label="Iles">
+          <option value="Port-cros">Port-Cros</option>
+          <option value="Porquerolles">Porquerolles</option>
+          <option value="Ile du Levant">île du Levant</option>
+        </optgroup>
+      </select>
+
+      <!--input départ-->
+
+      <div class="champ">
+        <input type="text" id="départ" name="départ" maxlength="30" placeholder="Ville ou lieu de départ" autocomplete="">
+      </div>
+
+      <!--input distance-->
+
+      <div class="champ">
+        <input type="text" id="distance" name="distance" maxlength="5" placeholder="max km" autocomplete="">
+      </div>
+
+      <!--input action-->
+
+      <div class="champ">
+        <input type="text" id="action" name="action" maxlength="30" placeholder="Actions" autocomplete="">
+      </div>
+
+      <!--input note-->
+
+      <div class="champ">
+        <input type="text" id="note" name="note" maxlength="30" placeholder="Note" autocomplete="">
+      </div>
+
+      <!--Bonton RECHERCHE-->
+
+      <div class="champ">
+        <input type="submit" value="RECHERCHE">
+      </div>
+
+    </form>
   </div>
+
+  <!--Résultat(tableau)-->
 
   <div class="TableauListesMer">
 
@@ -29,10 +113,10 @@
           <td>27 km</td>
           <td>
             <ul>
-            <li>Action n°1</li>
-            <li>Action n°2</li>
-          </ul>
-        </td>
+              <li>Action n°1</li>
+              <li>Action n°2</li>
+            </ul>
+          </td>
           <td class="Moyenne">
             <img src="img/DauphinBleu.png" alt="Dauphin Bleu;">
             <img src="img/DauphinBleu.png" alt="Dauphin Bleu;">
@@ -74,8 +158,7 @@
       </tbody>
     </table>
 
-    <form action="" method="post">
-    </form>
+
 
   </div>
 </section>
