fc0b0facdc1e7c485d30342a16ad92540cf97644
Damien Broggini <damienbroggini@hotmail.fr>
2020-08-31 09:38:09 +0200

    Codage modif header 31/08/2020

---
+++ Terre/codageTerreV2/footer.php
@@ -0,0 +1,48 @@
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/footer_styles.css">
+        <title>Document</title>
+    </head>
+    <body>
+    <footer class="footer container-fluid">
+        <div class="row">
+            <div class="title col-xl-3 col-lg-3 col-md-3 col-sm-3 ">
+                <p>Ecologie<br>Tourisme</p>
+            </div>
+            <div class="reseaux-sociaux col-xl-6 col-lg-6 col-md-6 col-sm-6  ">
+                <a href="#" class="mr-4"><img src="img/fb.svg" alt="facebook"></a>
+                <a href="#" class="mr-4"><img src="img/lin.svg" alt="lin"></a>
+                <a href="#" class="mr-4"><img src="img/wa.svg" alt="what's app"></a>
+                <a href="#" class="mr-4"><img src="img/email.jpg" alt="email var nature"></a>
+            </div>
+			<div class="container_logo col-xl-2 col-lg-2 col-md-2 col-sm-2" >
+            <a href="#"><img class="img-fluid" src="img/logo.png" alt="Logo var nature"></a>
+            </div>
+        </div>
+
+    </footer>
+
+    </body>
+    </html>
+    
+    
+    
+
+
+
+
+
+
+
+    <!-- JAVASCRIPT BOOTSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Terre/codageTerreV2/terreV7.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terre</title>
+    <link rel="stylesheet" href="css/terreV7.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!--FONT-->
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@900&display=swap" rel="stylesheet"> 
+
+</head>
+<body>
+    <?php
+    include "header.php";
+    ?>
+        <div class="bgHeader">
+            
+            <div class="offset-xl-1 col-xl-10 rectangleBlancHeader">
+                <!-- <div class="rectangleBlancHeader"> -->
+                <div class="contenuJumbotron">
+                    <img src="img/terreEcolo.png" alt="terre ecologique" class="terreEcolo">
+                    <!-- <div class="contenuJumbotron"></div> -->
+                    <div class="p-5 col-lg-11">
+                        <h1 class="display-4 text-right font-weight-bold marginTextJumbotron laTerreEcologique">La terre écologique</h1>
+                        <p class="lead font-weight-bold text-right">This is a simple hero unit, a simple jumbotron-style component<br> for calling extra attention to featured content or information.<br>
+                            Paphius quin etiam et Corneliu. <!--senatores,--> <br><!--ambo venenorum artibus pravis se polluisse confessi, eodem pronuntiante Maximino--> </p>
+                        <!-- <a class="btn btn-outline-danger btn-lg float-right btnJumbotron" href="#" role="button">Découvrir</a> -->
+                        <button type="button" class="btn btn-outline-dark btn-lg font-weight-bold float-right btnJumbotron">Découvrir</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p class="titrePrincipalTerre offset-xl-1 offset-lg-1 offset-md-1">TERRE</p>
+            </div>
+        </div>
+    <!--------BODY DE LA PAGE------------>
+    <!--FORET-->
+    <section class="responsiveImage575px">
+        <div class="d-flex justify-content-center mt-5 ">
+            <img src="img/quatrePoints.png" alt="quatre points section ecologique" class="quatrePointsResponsive ">
+        </div>
+        <div class="mt-5 blocForetResponsive">
+            
+                <img src="img/foret.jpg" alt="foret ecologique" class="offset-xl-1 col-xl-5 photoForetDessous">
+                <img src="img/foret.jpg" alt="foret ecologique" class="offset-xl-2 col-xl-5 col-md-12 photoForetDessus photoForetResponsive">
+            
+            <div class="float-right p-5 blocTexteForet blocTexteForetResponsive">
+                <div class="titreLesActionEnForet">
+                    <h2 class="titreLesActionsEn">Les actions en</h2>
+                    <h1 class="titreForet">Forêt</h1>
+                </div>
+                <div class="rectangleBlancResponsive"></div>
+                <div class="paragrapheButton">
+                    <p class="paragrapheForet">Cursibus potentissimorumque regionum <br>passibus ante nec gestas cursibus celeritate<br>
+                        ebris potuisse potentissimorumque res<br> proeliorum usurpare nec populorum passibus
+                        nec oculos </p>
+                    <button type="button" class="btn btn-outline-dark btn-lg font-weight-bold float-left btnJumbotron btnForetResponsive">Découvrir</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--TERRE-->
+    <section class="sectionTerreResponsive">
+        <div class="d-flex justify-content-center mt-5">
+            <img src="img/quatrePoints.png" alt="quatre point section ecologique" class="quatrePointsResponsive">
+        </div>
+        <div class="container-fluid mt-5">
+            <div class="row">
+                <div class="rectangleBlancTerreResponsive"></div>
+                <div class="titreButtonTerreResponsive">
+                    <div class="titreLesActionSurTerre">
+                        <h2 class="titreLesActionsSur">Les actions sur la</h2>
+                        <h1 class="titreTerre">Terre</h1>
+                    </div>
+                    <div class="paragrapheButtonTerre">
+                        <p class="paragrapheTerre text-right">Cursibus potentissimorumque regionum <br>passibus ante nec gestas cursibus celeritate<br>
+                            ebris potuisse potentissimorumque res<br> proeliorum usurpare nec populorum passibus
+                            nec oculos </p>
+                        <button type="button" class="btn btn-outline-dark btn-lg font-weight-bold btnJumbotron float-right">Découvrir</button>
+                    </div>
+                </div>
+            
+                <div class="imgTerre">
+                    <img src="img/terre.jpg" alt="foret ecologique" class="photoTerreDessous offset-xl-5 col-xl-6 offset-xl-2">
+                    <img src="img/terre.jpg" alt="foret ecologique" class="photoTerreDessus col-xl-5 photoTerreResponsive">
+                </div>
+            </div>
+        </div>
+    </section>
+        <!--VILLE-->
+    <section class="ville">
+        <div class="d-flex justify-content-center mt-5">
+            <img src="img/quatrePoints.png" alt="quatre point section ecologique" class="quatrePointsResponsive">
+        </div>
+        <div class="imageResponsive575px mt-5">
+            
+                <img src="img/ville.jpg" alt="ville ecologique" class="offset-xl-1 col-xl-5 photoVilleDessous">
+                <img src="img/ville.jpg" alt="ville ecologique" class="offset-xl-2 col-xl-5 photoVilleDessus photoVilleResponsive">
+            
+            <div class="float-right p-5 blocTexteVille">
+                <div class="titreLesActionEnVille">
+                    <h2 class="titreVilleLesActionsEn">Les actions en</h2>
+                    <h1 class="titreVille">Ville</h1>
+                </div>
+                <div class="rectangleBlancVilleResponsive"></div>
+                <div class="paragrapheButtonVille">
+                    <p class="paragrapheVille">Cursibus potentissimorumque regionum <br>passibus ante nec gestas cursibus celeritate<br>
+                        ebris potuisse potentissimorumque res<br> proeliorum usurpare nec populorum passibus
+                        nec oculos </p>
+                    <button type="button" class="btn btn-outline-dark btn-lg font-weight-bold float-left btnJumbotron">Découvrir</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="commentaires">
+        
+        <div class="flecheNone">
+            <img src="img/fleche-verte.png" alt="fleche commentaires" class="w-25 offset-xl-4 col-xl-4">
+            <button type="button" class="btn btn-outline-dark btn-lg font-weight-bold btnJumbotron">+ Commentaires</button>
+        </div>     
+        
+
+    </section>
+        
+        <?php
+            include "footer.php";
+        ?>
+        
+    
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
+
