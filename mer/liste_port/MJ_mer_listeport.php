ffc1aaba6df333525685f4c9def7d4d02264fe35
Jérôme Moura <DWWM-Jerome@mode83.onmicrosoft.com>
2020-09-01 16:38:55 +0200

    Finalisation US

--- mer/liste_port/MJ_mer_listeport.php
+++ mer/liste_port/MJ_mer_listeport.php
@@ -9,55 +9,43 @@
     <body>
         <?php
         include_once 'element/MJ_mer_connexionbdd.php';
+        ?>
+        <div class="lport">
+            <div class="cadre_critere py-1">
+                <form action="MJ_mer_listeport.php" method="post">
+                    <label for="critere">Classez par :</label>
+                    <select id="critere" name="critere">
+                        <option value="nom">Nom</option>
+                        <option value="localisation">Lieu</option>
+                        <option value="pp">Label Port Propres</option>
+                        <option value="aeb">Label Actifs en Biodiversité</option>
+                        <option value="pb">Label Pavillon Bleu</option>
+                    </select>
+                    <input type="submit">
+                </form>
+                <?php
+                include 'element/MJ_mer_classement.php';
+                ?>
+            </div>
+
+        <?php
 
         //Collecte des données
-        $sql = $pdo->prepare("SELECT * FROM `jm_mer_liste_port`");
-        $sql->execute();
         $liste = $sql->fetchALL(PDO::FETCH_ASSOC);
 
         //Boucle d'affichage des données
         foreach ($liste as $i){ ?>
 
                 <!-- Affichage Nom du lieu-->
-                <h5><a type="button" class="lieu" data-toggle="modal" data-target="#Port<?php echo $i['lieu_id']; ?>">
+            <div class="affichage"> 
+                <h5><a type="button" class="lien pt-1" data-toggle="modal" data-target="#Port<?php echo $i['lieu_id']; ?>">
                     <?php echo $i['lieu']; ?>
                 </a></h5>
 
                 <p class="mb-1">Emplacement : <?php echo $i['localisation'] ?></p>
 
-                <p><?php 
-                //Condition pour l'affichage du logo port propre
-                switch($i['label_pp']) {
-                    case 1: 
-                    //Condition si le port est juste engagé?>
-                            <img src="img/1pp.svg" alt="Engagé Port Propre" width="30" height="30">
-                        <?php break;
-                    case 2: 
-                    //Condition si le port est certifié?>
-                            <img src="img/2pp.svg" alt="Certifié Port Propre" width="30" height="30">
-                        <?php break;
-                }
-                ?>
-
-                <?php 
-                //Condition pour l'affichage du logo actifs en biodiversité 
-                switch($i['label_aeb']) {
-                    case 1: 
-                    //Condition si le port est juste engagé?>
-                            <img src="img/1aeb.svg" alt="Engagé Actifs en Biodiversité" width="30" height="30">
-                        <?php break;
-                    case 2: 
-                    //Condition si le port est certifié?>
-                            <img src="img/2aeb.svg" alt="Certifié Actifs en Biodiversité" width="30" height="30">
-                        <?php break;
-                }
-                ?>
-
-                <?php
-                //Condition pour l'affichage du pavillon bleu
-                if($i['label_pb']){ ?>
-                    <img src="img/pb.png" alt="Pavillon Bleu" height="30">
-                <?php } ?>
+                <p class="mb-0 pb-1">
+                    <?php include 'element/MJ_mer_logoaction.php' ?>
                 </p>
 
                 <!-- Modal d'affichage contanant d'avantage d'information sur le lieu -->
@@ -77,24 +65,29 @@
                                     <div class="row">
                                         <div class="col-md">
                                             <p>
-                                                <a href="<?php echo $i['lien']?>" target="_blank">Site Officiel</a><br>
-                                                Localisation : <?php echo $i['localisation'] ?>
+                                                <a class="lien" href="<?php echo $i['lien']?>" target="_blank">Site Officiel</a><br>
+                                                Localisation : <?php echo $i['localisation'] ?> <br>
+                                                <?php include 'element/MJ_mer_logoaction.php' ?> <br>
+                                                <!-- Emplacement Vote -->
                                             </p>
                                         </div>
                                         <div class="col-md carte">
-
+                                        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $i['carte']?>" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=43.17920&amp;mlon=5.68141#map=17/43.17920/5.68141" target="_blank">Afficher une carte plus grande</a></small>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- Emplacement Commentaire -->
                             </div>
                         </div>
                     </div>
                 </div>
-
+            </div>
 
         <?php
         }
         ?>
+
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
--- mer/liste_port/js/MJ_mer_listeport.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < 41; i++) {
-    document.getElementsByClassName("lieu")[i].addEventListener("click", displayMap);
-    function displayMap() {
-        document.getElementsByClassName("carte")[i].innerHTML = "Hello <?php echo $i['lieu_id']; ?>"/*"<iframe width=\"425\" height=\"350\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"<?php echo $i['carte']?>\" style=\"border: 1px solid black\"></iframe><br/><small><a href=\"https://www.openstreetmap.org/?mlat=43.17920&amp;mlon=5.68141#map=17/43.17920/5.68141\" target=\"_blank\">Afficher une carte plus grande</a></small>"*/;
-    }
-}
