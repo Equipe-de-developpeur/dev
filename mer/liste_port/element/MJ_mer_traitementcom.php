e52c4ba13807f21db0284f638adb14af4c6fa05c
Jérôme Moura <DWWM-Jerome@mode83.onmicrosoft.com>
2020-09-04 16:40:33 +0200

    MAJ sécurite insertion

--- mer/liste_port/element/MJ_mer_traitementcom.php
+++ mer/liste_port/element/MJ_mer_traitementcom.php
@@ -1,12 +1,18 @@
 <?php
 
+function securite($donnee) {
+    $donnee = htmlspecialchars($donnee);
+    $donnee = trim($donnee);
+    $donnee = stripslashes($donnee);
+    $donnee = str_replace("'", "\'", $donnee);
+    return $donnee;
+}
+
 if (isset($_POST["username"])) {
 
     $ID = $_POST["id"];
-    $username = $_POST["username"];
-    $precommentaire = $_POST["commentaire"];
-
-    $commentaire = str_replace("'", "\'", $precommentaire);
+    $username = securite($_POST["username"]);
+    $commentaire = securite($_POST["commentaire"]);
 
     $insertcom = $pdo->prepare("INSERT INTO `jm_mer_commentaire_port`(`lieu_id`, `username`, `commentaire`) VALUES ('$ID', '$username', '$commentaire')");
     $insertcom->execute();
