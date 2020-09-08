c8f3141095082b1e5bd2849a3688729f0ab34838
unknown <jeremvk@outlook.fr>
2020-09-08 12:30:32 +0200

    Debut inscription gite

--- Hebergement/functions_gite.php
+++ Hebergement/functions_gite.php
@@ -2,4 +2,10 @@
     function debug($variable){
         echo '<pre>' .print_r($variable, true). '</pre>';
     }
+
+    function str_random($length){
+        $alphabet = "123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+        // On ne veut que 60 caractère, on melange, on repète la variable $alphabet le nombre de $length (ici 60 fois)
+        substr(str_shuffle(str_repeat($alphabet, $length)),0, $length);
+    }
 ?>
