c2522006dbe37af9a92cb75c14a247a032eee3e0
JeromeMouraDWWM <DWWM-Jerome@mode83.onmicrosoft.com>
2020-09-11 11:12:12 +0200

    Correction nom bdd et lien menu

---
+++ mer/liste_port/element/MJ_mer_affichagecom.php
@@ -0,0 +1,28 @@
+<div class="commentaire p-1">
+    <div class="row">
+        <!-- Affichage nom utilisateur -->
+        <div class="col col-6">
+            <?php echo $j['commentaire_port_username']; ?>
+        </div>
+        <!-- Affichage date de publication -->
+        <div class="col col-6 text-right">  
+            <small><?php echo $j['commentaire_port_temps']; ?></small>
+        </div>
+    </div>
+    <!-- Affichage du commentaire -->
+    <div class="row">
+        <div class="col"><?php echo $j['commentaire_port_commentaire']; ?></div>
+    </div>
+    <!-- Affichage du fichier envoyé -->
+    <?php
+    if($j['commentaire_port_fichier'] != NULL) {
+        ?>
+        <div class="row">
+            <div class="col text-right">
+                <small><a href="fichier/<?php echo $lieuID . '/' . $j['commentaire_port_fichier']; ?>"><?php echo $j['commentaire_port_fichier']; ?></a></small>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+</div>
