b98f61e2574006554ca19031efa602818a2d208c
Aurélien <dupuis.aurelien.eps@outlook.fr>
2020-09-17 16:20:19 +0200

    rectification value et id du formulaire

--- mer/Plage_liste/DA_mer_plage_liste_details.php
+++ mer/Plage_liste/DA_mer_plage_liste_details.php
@@ -13,20 +13,20 @@
 
 <body>
 
-  <?php 
-  
+  <?php
+
   include "DA_mer_plage_liste_bdd.php";
   include "DA_mer_plage_liste_detail_vote.php";
 
   $id = $_REQUEST['id'];
-  $req = $connexion->query("SELECT * FROM da_liste_plage WHERE `liste_plage_id`=$id"); 
-$plageliste = new PlageListe;
-if(isset($_REQUEST['VOTER']))
-{
-  $plageliste->notation();
-  header('Location: '.$_SERVER['HTTP_REFERER'] );//Rafraichir la page si il y a 1 vote existant
-}
-   while ($donnees = $req->fetch()) {
+  $req = $connexion->query("SELECT * FROM da_liste_plage WHERE `liste_plage_id`=$id");
+
+  $plageliste = new PlageListe;
+  if (isset($_REQUEST['VOTER'])) {
+    $plageliste->notation();
+    header('Location: ' . $_SERVER['HTTP_REFERER']); //Rafraichir la page si il y a 1 vote existant
+  }
+  while ($donnees = $req->fetch()) {
     // Enregistrement des données sous forme de variables
     $liste_plage_id = $donnees['liste_plage_id'];
     $lieux = $donnees['liste_plage_lieux'];
@@ -68,72 +68,78 @@ if(isset($_REQUEST['VOTER']))
   
     <div class="ligne3">
       <div class="moyenne">
-        <p><span>Note moyenne (nombre de vote :'. $nombre_de_vote.' )</span></p>
+        <p><span>Note moyenne (nombre de vote :' . $nombre_de_vote . ' )</span></p>
         <div class="test1">');
-        if($note_moyenne==0){
-          echo('
+    if ($note_moyenne == 0) {
+      echo ('
           <img src="img/DauphinGris.png" alt="Dauphin gris">
             <img src="img/DauphinGris.png" alt="Dauphin gris">
             <img src="img/DauphinGris.png" alt="Dauphin gris"> 
             <img src="img/DauphinGris.png" alt="Dauphin gris">
             <img src="img/DauphinGris.png" alt="Dauphin gris">
-          ');}
-        if($note_moyenne<=1.5 && $note_moyenne>0){
-        echo('
+          ');
+    }
+    if ($note_moyenne <= 1.5 && $note_moyenne > 0) {
+      echo ('
           <img src="img/DauphinBleu.png" alt="Dauphin bleu">
           <img src="img/DauphinGris.png" alt="Dauphin gris">
           <img src="img/DauphinGris.png" alt="Dauphin gris"> 
           <img src="img/DauphinGris.png" alt="Dauphin gris">
           <img src="img/DauphinGris.png" alt="Dauphin gris">
-        ');}
-        if($note_moyenne>1.5 && $note_moyenne<=2.2){
-          echo('
+        ');
+    }
+    if ($note_moyenne > 1.5 && $note_moyenne <= 2.2) {
+      echo ('
           <img src="img/DauphinBleu.png" alt="Dauphin bleu">
           <img src="img/DauphinBleu.png" alt="Dauphin bleu">
             <img src="img/DauphinGris.png" alt="Dauphin gris">
             <img src="img/DauphinGris.png" alt="Dauphin gris">
             <img src="img/DauphinGris.png" alt="Dauphin gris">
 
-          ');}
-          if($note_moyenne>2.2 && $note_moyenne<=3){
-            echo('
+          ');
+    }
+    if ($note_moyenne > 2.2 && $note_moyenne <= 3) {
+      echo ('
             <img src="img/DauphinBleu.png" alt="Dauphin bleu">
             <img src="img/DauphinBleu.png" alt="Dauphin bleu">
             <img src="img/DauphinBleu.png" alt="Dauphin bleu">
               <img src="img/DauphinGris.png" alt="Dauphin gris">
               <img src="img/DauphinGris.png" alt="Dauphin gris">
-            ');}
-            if($note_moyenne>3 && $note_moyenne<=4){
-              echo('
+            ');
+    }
+    if ($note_moyenne > 3 && $note_moyenne <= 4) {
+      echo ('
               <img src="img/DauphinBleu.png" alt="Dauphin bleu">
               <img src="img/DauphinBleu.png" alt="Dauphin bleu">
               <img src="img/DauphinBleu.png" alt="Dauphin bleu">
               <img src="img/DauphinBleu.png" alt="Dauphin bleu">
                 <img src="img/DauphinGris.png" alt="Dauphin gris">
     
-              ');}
-              if($note_moyenne>4){
-                echo('
+              ');
+    }
+    if ($note_moyenne > 4) {
+      echo ('
                 <img src="img/DauphinBleu.png" alt="Dauphin bleu">
                 <img src="img/DauphinBleu.png" alt="Dauphin bleu">
                 <img src="img/DauphinBleu.png" alt="Dauphin bleu">
                 <img src="img/DauphinBleu.png" alt="Dauphin bleu">
                 <img src="img/DauphinBleu.png" alt="Dauphin bleu">
-                ');}
+                ');
+    }
 
-                
 
-        echo('
+
+    echo ('
         </div>
       </div>
   
       <div class="avis">
         <p><span>Votre avis :</span></p>');
 
-        /* note = 0 (pas encore de vote) */
+    /* note = 0 (pas encore de vote) */
 
-        if($votre_avis==0){
-          echo('
+    if ($votre_avis == 0) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -161,13 +167,13 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="VOTER" name="VOTER">
 </form>
           ');
-        }
+    }
 
 
-        /* note = 1 */
+    /* note = 1 */
 
-        if($votre_avis==1){
-          echo('
+    if ($votre_avis == 1) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -195,12 +201,12 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="MODIFIER" name="VOTER">
 </form>
           ');
-        }
+    }
 
-        /* note = 2 */
+    /* note = 2 */
 
-        if($votre_avis==2){
-          echo('
+    if ($votre_avis == 2) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -210,8 +216,8 @@ if(isset($_REQUEST['VOTER']))
             <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
             <input class="c" type="radio" name="note" id="note1" value="1">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" checked name="note" id="note1" value="1">
+            <label for="note2" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" checked name="note" id="note2" value="2">
          
         <label for="note3" class="labelButon"><img class="A2" src="img/DauphinGris.png" alt="Dauphin gris"></label>
         <input class="c" type="radio" name="note" id="note3" value="3">
@@ -228,12 +234,12 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="MODIFIER" name="VOTER">
 </form>
           ');
-        }
+    }
 
-        /* note = 3 */
+    /* note = 3 */
 
-        if($votre_avis==3){
-          echo('
+    if ($votre_avis == 3) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -243,11 +249,11 @@ if(isset($_REQUEST['VOTER']))
             <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
             <input class="c" type="radio" name="note" id="note1" value="1">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note2" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note2" value="2">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" checked name="note" id="note1" value="1">
+            <label for="note3" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" checked name="note" id="note3" value="3">
          
         <label for="note4" class="labelButon"><img class="A2" src="img/DauphinGris.png" alt="Dauphin gris"></label>
         <input class="c" type="radio" name="note" id="note4" value="4">
@@ -261,12 +267,12 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="MODIFIER" name="VOTER">
 </form>
           ');
-        }
+    }
 
-        /* note = 4 */
+    /* note = 4 */
 
-        if($votre_avis==4){
-          echo('
+    if ($votre_avis == 4) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -276,14 +282,14 @@ if(isset($_REQUEST['VOTER']))
             <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
             <input class="c" type="radio" name="note" id="note1" value="1">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note2" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note2" value="2">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note3" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note3" value="3">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" checked name="note" id="note1" value="1">
+            <label for="note4" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" checked name="note" id="note4" value="4">
          
         <label for="note5" class="labelButon"><img class="A2" src="img/DauphinGris.png" alt="Dauphin gris"></label>
         <input class="c" type="radio" name="note" id="note5" value="5">
@@ -294,12 +300,12 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="MODIFIER" name="VOTER">
 </form>
           ');
-        }
+    }
 
-        /* note = 5 */
+    /* note = 5 */
 
-        if($votre_avis==5){
-          echo('
+    if ($votre_avis == 5) {
+      echo ('
           <div class="test4">
           <form action="" method="post">
             <div class="note">
@@ -309,17 +315,17 @@ if(isset($_REQUEST['VOTER']))
             <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
             <input class="c" type="radio" name="note" id="note1" value="1">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note2" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note2" value="2">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note3" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note3" value="3">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" name="note" id="note1" value="1">
+            <label for="note4" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" name="note" id="note4" value="4">
          
-            <label for="note1" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
-            <input class="c" type="radio" checked name="note" id="note1" value="1">
+            <label for="note5" class="labelButon"><img class="A2" src="img/DauphinBleu.png" alt="Dauphin Bleu"></label>
+            <input class="c" type="radio" checked name="note" id="note5" value="5">
          
      
 </div>
@@ -327,8 +333,8 @@ if(isset($_REQUEST['VOTER']))
 <input type="Submit" value="MODIFIER" name="VOTER">
 </form>
           ');
-        }
-           echo('
+    }
+    echo ('
 
 
         </div>
